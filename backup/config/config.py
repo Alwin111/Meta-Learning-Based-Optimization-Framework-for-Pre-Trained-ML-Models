68839650e00953fff6d7481be3dda735b0df1044
Alwin111 <ovoxo890@gmail.com>
2026-04-19 14:08:02 +0530

    Clean project for submission

--- backup/config/config.py
+++
@@ -1,7 +0,0 @@
-MODEL_PATH = "user_input/model/model.onnx"
-
-DATASET_PATH = "data/processed/cifar10/X_test.npy"
-
-NUM_BENCHMARK_SAMPLES = 50
-
-QUANT_MODEL_PATH = "user_input/model/model_quantized.onnx"
--- backup/inference.py
+++
@@ -1,7 +0,0 @@
-import time
-
-def run_inference(model, X):
-    start = time.time()
-    preds = model.predict(X)
-    latency = time.time() - start
-    return preds, latency
