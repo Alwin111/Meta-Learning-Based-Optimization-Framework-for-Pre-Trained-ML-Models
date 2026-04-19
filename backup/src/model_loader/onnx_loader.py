6ab82ae0d615f85963b8b6935a2829ecf1075cb3
Alwin111 <ovoxo890@gmail.com>
2026-04-19 14:08:02 +0530

    Clean project for submission

--- backup/src/model_loader/onnx_loader.py
+++
@@ -1,9 +0,0 @@
-import onnxruntime as ort
-
-
-def load_onnx_model(model_path):
-    """
-    Loads an ONNX model using ONNX Runtime.
-    """
-    session = ort.InferenceSession(model_path)
-    return session
